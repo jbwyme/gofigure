34a47b60ee39d470633486f867c15892224bb2b1
Josh Wymer <wymetyme@gmail.com>
2015-07-20 00:30:53 -0700

    allow batch writes

--- write.go
+++ write.go
@@ -3,6 +3,7 @@ package main
 // Go provides a `flag` package supporting basic
 // command-line flag parsing. We'll use this package to
 // implement our example command-line program.
+import "encoding/json"
 import "flag"
 import "fmt"
 import "os"
@@ -16,6 +17,29 @@ func main() {
 	if actionJson, err := simplejson.NewJson([]byte(data)); err != nil {
                 panic(err)
         } else {
+		actionArr, err := actionJson.Array()
+		if err == nil {
+			for _, action := range actionArr {
+				jsonString, err := json.Marshal(action)
+				if err != nil {
+					fmt.Println(err)
+				}
+		
+				writeAction(string(jsonString))
+			} 
+		} else {
+			action, err := actionJson.MarshalJSON()
+			if err == nil {
+				writeAction(string(action))
+			} else {
+				panic(err)
+			}
+		}
+	}
+}
+
+func writeAction(action string) {
+		actionJson, err := simplejson.NewJson([]byte(action))
 		ts, _ := actionJson.Get("_ts").Int()
 		t := time.Unix(int64(ts), 0)
 		file := fmt.Sprintf("data/%d-%02d-%02dT%02d", t.Year(), t.Month(), t.Day(), t.Hour())
@@ -32,8 +56,7 @@ func main() {
 		}
 
 		defer f.Close()
-		if _, err = f.WriteString(data + "\n"); err != nil {
+		if _, err = f.WriteString(action + "\n"); err != nil {
 		    panic(err)
 		}	
-	}
 }
